1b4d2f6df60bb96f221870cafe4975c123412ece
Tegbaru16 <90245505+Tegbaru16@users.noreply.github.com>
2022-06-16 15:17:55 -0800

    Update up_drawer.dart
    Hi Guys! please check other darts and modify this dart.

--- calculator/lib/widgets/app_drawer.dart
+++ calculator/lib/widgets/app_drawer.dart
@@ -1,5 +1,6 @@
 import 'package:converter/main.dart';
 import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
 
 class AppDrawer extends StatelessWidget {
   const AppDrawer({Key? key}) : super(key: key);
@@ -13,7 +14,41 @@ class AppDrawer extends StatelessWidget {
           Container(
             padding:const EdgeInsets.all(28),
             child:Column(children: [ 
-              
+              InkWell(
+                onTap: () => {
+                  Navigator.of(context).pushNamed('/')
+                },
+                child: Row(
+                          children: 
+                          const [Icon(Icons.home), 
+                          SizedBox(width: 15, height: 60,), 
+                          Text('Home')],
+                           ),
+              ),
+              const Divider(),
+               InkWell(
+                onTap: () => {
+                  Navigator.of(context).pushNamed('/about-us')
+                },
+                child: Row(
+                          children: 
+                          const [Icon(Icons.phone), 
+                          SizedBox(width: 15, height: 60,), 
+                          Text('About Us')],
+                           ),
+              ),
+              InkWell(
+               onTap: () => {
+                 Navigator.of(context).pushNamed('/fav-items')
+               },
+               child: Row(
+                 children: const [
+                   Icon(Icons.emoji_emotions), 
+                   SizedBox(width: 15, height: 60,), 
+                   Text('GitHub')],
+               ),
+             ),
+            
              ],)
              
           )
