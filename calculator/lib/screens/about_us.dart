86c45665924e43ec52ae6467f09c4947f131fe4c
Naol Fekadu <89836698+4ole@users.noreply.github.com>
2022-06-19 20:00:32 +0300

    final about us update

--- calculator/lib/screens/about_us.dart
+++ calculator/lib/screens/about_us.dart
@@ -3,14 +3,19 @@ import 'package:typicons_flutter/typicons_flutter.dart';
 import '../widgets/app_drawer.dart';
 
 class AboutUs extends StatelessWidget {
+
    static const routeName='/about-us';
-   const AboutUs({Key? key}) : super(key: key);
+
+  const AboutUs({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
+   
     return Scaffold(
       appBar:AppBar(title:const Text("About Us")),
-      drawer: const AppDrawer(),
-      body: Center(
+      
+     drawer: const AppDrawer(),
+       body: Center(
             child: Container(
               
                 child: Text(
