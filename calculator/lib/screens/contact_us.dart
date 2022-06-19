5670e2f4ecd558ae2e7edce7e63c185dd12b1baa
EyuelAlemayehu <89838394+EyuelAlemayehu@users.noreply.github.com>
2022-06-19 20:07:24 +0300

    Update contact_us.dart

--- calculator/lib/screens/contact_us.dart
+++ calculator/lib/screens/contact_us.dart
@@ -1,16 +1,22 @@
 library contactus;
+
 import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:typicons_flutter/typicons_flutter.dart';
 import 'package:url_launcher/url_launcher.dart';
 import '../widgets/app_drawer.dart';
 
+
 ///Class for adding contact details/profile details as a complete new page in your flutter app.
 class ContactUs extends StatelessWidget {
 
   static const routeName='/contact-us';
+
+    
+
   final String? phoneNumber;
-  
+
   ///Text for Phonenumber
   final String? phoneNumberText;
 
@@ -69,13 +75,17 @@ class ContactUs extends StatelessWidget {
 
   /// avatar radius will place the circularavatar according to developer/UI need
   final double? avatarRadius;
+
+  ///Constructor which sets all the values.
   ContactUs({
     required this.companyName,
    
     required this.email,
     this.emailText,
+   
     required this.phoneNumber,
-    this.phoneNumberText,    
+    this.phoneNumberText,
+    
     required this.twitterHandle,
     required this.facebookHandle,
     required this.linkedinURL,
@@ -92,276 +102,3 @@ class ContactUs extends StatelessWidget {
     this.avatarRadius,
     this.dividerThickness,
   });
-  showAlert(BuildContext context) {
-    
-   
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-       
-          elevation: 8.0,
-         
-          contentPadding: EdgeInsets.all(18.0),
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
-          content: Container(
-            
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                InkWell(
-                  onTap: () => launch('tel:' + phoneNumber!),
-                  child: Container(
-                    height: 50.0,
-                    alignment: Alignment.center,
-                    child: Text('Call'),
-                  ),
-                ),
-                    Divider(),
-                
-                InkWell(
-                  onTap: () => launch('sms:' + phoneNumber!),
-                  child: Container(
-                    alignment: Alignment.center,
-                    height: 50.0,
-                    child: Text('Message'),
-                  ),
-                ),
-                Divider(),
-                InkWell(
-                  onTap: () => launch('https://wa.me/' + phoneNumber!),
-                  child: Container(
-                    alignment: Alignment.center,
-                    height: 50.0,
-                    child: Text('WhatsApp'),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        );
-      },
-    );
-  }
-@override
-  Widget build(BuildContext context) {
- 
-    return Scaffold(
-      appBar:AppBar(title:const Text("Contact Us")),
-      
-     drawer: const AppDrawer(),
-    body: 
-    Padding(
-        padding: EdgeInsets.all(16),
-        
-        child: SafeArea(
-        
-        child: SingleChildScrollView(
-          
-          child: Column(
-            
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-           
-              Text(
-                companyName,
-                style: TextStyle(
-                  fontFamily: companyFont ?? 'Pacifico',
-                  fontSize: companyFontSize ?? 40.0,
-                 
-                  fontWeight: companyFontWeight ?? FontWeight.bold,
-                ),
-              ),
-              Visibility(
-                visible: tagLine != null,
-                child: Text(
-                  tagLine ?? "",
-                  style: TextStyle(
-                    fontFamily: taglineFont ?? 'Pacifico',
-                 
-                    fontSize: 20.0,
-                    letterSpacing: 2.0,
-                    fontWeight: taglineFontWeight ?? FontWeight.bold,
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 10.0,
-              ),
-              Divider(
-                color: dividerColor ?? Colors.teal[200],
-                thickness: dividerThickness ?? 4.0,
-                indent: 50.0,
-                endIndent: 50.0,
-              ),
-              SizedBox(
-                height: 10.0,
-              ),
-              
-              Visibility(
-                visible: phoneNumber != null,
-                child: Card(
-                  clipBehavior: Clip.antiAlias,
-                  margin: EdgeInsets.symmetric(
-                    vertical: 10.0,
-                    horizontal: 25.0,
-                  ),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(50.0),
-                  ),
-                  child: ListTile(
-                    leading: Icon(Typicons.phone),
-                    title: Text(
-                      phoneNumberText ?? 'Phone Number',
-                      style: TextStyle(
-                        
-                        fontFamily: textFont,
-                      ),
-                    ),
-                    onTap: () => showAlert(context),
-                  ),
-                ),
-              ),
-Card(
-                clipBehavior: Clip.antiAlias,
-                margin: EdgeInsets.symmetric(
-                  vertical: 10.0,
-                  horizontal: 25.0,
-                ),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(50.0),
-                ),
-  child: ListTile(
-                  leading: Icon(Typicons.mail),
-                  title: Text(
-                    emailText ?? 'Email ID',
-                    style: TextStyle(
-                      
-                      fontFamily: textFont,
-                    ),
-                  ),
-                  onTap: () => launch('mailto:' + email),
-                ),
-              ),
-                 Visibility(
-                visible: facebookHandle != null,
-                child: Card(
-                  clipBehavior: Clip.antiAlias,
-                  margin: EdgeInsets.symmetric(
-                    vertical: 10.0,
-                    horizontal: 25.0,
-                  ),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(50.0),
-                  ),
-                    child: ListTile(
-                    leading: Icon(Typicons.social_facebook),
-                    title: Text(
-                      'Facebook',
-                      style: TextStyle(
-                       
-                        fontFamily: textFont,
-                      ),
-                    ),
-                    onTap: () =>
-                        launch('https://www.facebook.com/' + facebookHandle!),
-                  ),
-                ),
-              ),
-              
-                   Visibility(
-                visible: instagram != null,
-                child: Card(
-                  clipBehavior: Clip.antiAlias,
-                  margin: EdgeInsets.symmetric(
-                    vertical: 10.0,
-                    horizontal: 25.0,
-                  ),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(50.0),
-                  ),
-                 
-                  child: ListTile(
-                    leading: Icon(Typicons.social_instagram),
-                    title: Text(
-                      'Instagram',
-                      style: TextStyle(
-                       
-                        fontFamily: textFont,
-                      ),
-                    ),
-                    onTap: () => launch('https://instagram.com/' + instagram!),
-                  ),
-                ),
-              ),
-    Visibility(
-                visible: githubUserName != null,
-                child: Card(
-                  clipBehavior: Clip.antiAlias,
-                  margin: EdgeInsets.symmetric(
-                    vertical: 10.0,
-                    horizontal: 25.0,
-                  ),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(50.0),
-                  ),
-                  
-                  child: ListTile(
-                    leading: Icon(Typicons.social_github),
-                    title: Text(
-                      'Github',
-                      style: TextStyle(
-                       
-                        fontFamily: textFont,
-                      ),
-                    ),
-                    onTap: () => launch('https://github.com/marro-hub/calculator' + githubUserName!),
-                  ),
-                ),
-              ), 
-              
-              
-              Visibility(
-                visible: linkedinURL != null,
-                child: Card(
-                  clipBehavior: Clip.antiAlias,
-                  margin: EdgeInsets.symmetric(
-                    vertical: 10.0,
-                    horizontal: 25.0,
-                  ),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(50.0),
-                  ),
-                  
-                  child: ListTile(
-                    leading: Icon(Typicons.social_linkedin),
-                    title: Text(
-                      'Linkedin',
-                      style: TextStyle(
-                       
-                        fontFamily: textFont,
-                      ),
-                    ),
-                    onTap: () => launch(linkedinURL!),
-                  ),
-                ),
-              ),
-               Align(
-          alignment: Alignment.bottomCenter,
-          child: Text("\n\n  Get In Touch!  ",
-            
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30 ,backgroundColor:Colors.greenAccent)
-        )),
-            ],
-          ),
-        ),
-      ),
-      
-      
-      )
-      
-    );
-  }
-}
