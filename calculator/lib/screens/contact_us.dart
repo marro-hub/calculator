09f03366c26a696861325676dfa8007fade11f5b
EyuelAlemayehu <89838394+EyuelAlemayehu@users.noreply.github.com>
2022-06-19 20:08:28 +0300

    Update contact_us.dart

--- calculator/lib/screens/contact_us.dart
+++ calculator/lib/screens/contact_us.dart
@@ -102,3 +102,140 @@ class ContactUs extends StatelessWidget {
     this.avatarRadius,
     this.dividerThickness,
   });
+showAlert(BuildContext context) {
+    
+   
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+       
+          elevation: 8.0,
+         
+          contentPadding: EdgeInsets.all(18.0),
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
+          content: Container(
+            
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                InkWell(
+                  onTap: () => launch('tel:' + phoneNumber!),
+                  child: Container(
+                    height: 50.0,
+                    alignment: Alignment.center,
+                    child: Text('Call'),
+                  ),
+                ),
+                
+                
+    
+                Divider(),
+                
+                InkWell(
+                  onTap: () => launch('sms:' + phoneNumber!),
+                  child: Container(
+                    alignment: Alignment.center,
+                    height: 50.0,
+                    child: Text('Message'),
+                  ),
+                ),
+                Divider(),
+                InkWell(
+                  onTap: () => launch('https://wa.me/' + phoneNumber!),
+                  child: Container(
+                    alignment: Alignment.center,
+                    height: 50.0,
+                    child: Text('WhatsApp'),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+ 
+    return Scaffold(
+      appBar:AppBar(title:const Text("Contact Us")),
+      
+     drawer: const AppDrawer(),
+    body: 
+    Padding(
+        padding: EdgeInsets.all(16),
+        
+        child: SafeArea(
+        
+        child: SingleChildScrollView(
+          
+          child: Column(
+            
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+           
+              Text(
+                companyName,
+                style: TextStyle(
+                  fontFamily: companyFont ?? 'Pacifico',
+                  fontSize: companyFontSize ?? 40.0,
+                 
+                  fontWeight: companyFontWeight ?? FontWeight.bold,
+                ),
+              ),
+              Visibility(
+                visible: tagLine != null,
+                child: Text(
+                  tagLine ?? "",
+                  style: TextStyle(
+                    fontFamily: taglineFont ?? 'Pacifico',
+                 
+                    fontSize: 20.0,
+                    letterSpacing: 2.0,
+                    fontWeight: taglineFontWeight ?? FontWeight.bold,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Divider(
+                color: dividerColor ?? Colors.teal[200],
+                thickness: dividerThickness ?? 4.0,
+                indent: 50.0,
+                endIndent: 50.0,
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              
+              Visibility(
+                visible: phoneNumber != null,
+                child: Card(
+                  clipBehavior: Clip.antiAlias,
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10.0,
+                    horizontal: 25.0,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50.0),
+                  ),
+                  
+                  child: ListTile(
+                    leading: Icon(Typicons.phone),
+                    title: Text(
+                      phoneNumberText ?? 'Phone Number',
+                      style: TextStyle(
+                        
+                        fontFamily: textFont,
+                      ),
+                    ),
+                    onTap: () => showAlert(context),
+                  ),
+                ),
+              ),
