6baeb26de9f99e77451ed555bcb65cad37343b39
Tegbaru16 <90245505+Tegbaru16@users.noreply.github.com>
2022-06-18 16:37:52 -0800

    Update Contact_us.dart
    try also

--- calculator/lib/screens/contact_us.dart
+++ calculator/lib/screens/contact_us.dart
@@ -1,27 +1,77 @@
+library contactus;
+
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
+import 'package:typicons_flutter/typicons_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 import '../widgets/app_drawer.dart';
 
-class FavItemsScreen extends StatelessWidget {
 
-   static const routeName='/fav-items';
+///Class for adding contact details/profile details as a complete new page in your flutter app.
+class ContactUs extends StatelessWidget {
 
-  const FavItemsScreen({Key? key}) : super(key: key);
+  static const routeName='/contact-us';
 
-  @override
-  Widget build(BuildContext context) {
-   
-    return Scaffold(
-      appBar:AppBar(title:const Text("Github")),
-      
-     drawer: const AppDrawer(),
-      body: Padding(
-        padding: EdgeInsets.all(16),
-        child: Card(
-          child: Text("This is our git hub Url"),
-        )
-      
-      
-      ,)
-    );
-  }
-}
+    
+
+  final String? phoneNumber;
+
+  ///Text for Phonenumber
+  final String? phoneNumberText;
+
+ 
+
+  ///Email ID of company/individual
+  final String email;
+
+  ///Text for Email
+  final String? emailText;
+
+  ///Twitter Handle of Company/Individual
+  final String? twitterHandle;
+
+  ///Facebook Handle of Company/Individual
+  final String? facebookHandle;
+
+  ///Linkedin URL of company/individual
+  final String? linkedinURL;
+
+  ///Github User Name of the company/individual
+  final String? githubUserName;
+
+  ///Name of the Company/individual
+  final String companyName;
+
+  ///Font size of Company name
+  final double? companyFontSize;
+
+  ///TagLine of the Company or Position of the individual
+  final String? tagLine;
+
+  ///Instagram User Name of the company/individual
+  final String? instagram;
+
+  ///TextColor of the text which will be displayed on the card.
+  
+  /// font of text
+  final String? textFont;
+
+  /// font of the company/individul to be displayed
+  final String? companyFont;
+
+  /// font of the tagline to be displayed
+  final String? taglineFont;
+
+  /// divider color which is placed between the tagline & contact informations
+  final Color? dividerColor;
+
+  /// divider thickness which is placed between the tagline & contact informations
+  final double? dividerThickness;
+
+  ///font weight for tagline and company name
+  final FontWeight? companyFontWeight;
+  final FontWeight? taglineFontWeight;
+
+  /// avatar radius will place the circularavatar according to developer/UI need
+  final double? avatarRadius;
